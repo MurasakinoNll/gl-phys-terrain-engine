ab1530da5e5ca372dca5adb511171f9b1758a831
MurasakinoNll <meekacloneacc@gmail.com>
2025-11-12 21:49:58 +0300

    window init and header fix

---
+++ srcs/core/engine.c
@@ -0,0 +1,15 @@
+#include <curses.h>
+
+bool eng_init() {
+  // window init
+  // renderer init
+  return true;
+}
+
+void eng_run() {
+  // while(!window_should_close()){}
+}
+
+void eng_term() {
+  // window_term;
+}
--- srcs/core/window.c
+++ srcs/core/window.c
@@ -1,2 +1,25 @@
-#include <stdio.h>
-int main() { printf("test gl"); }
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+bool window_init() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "garakengine", NULL, NULL);
+  if (window == NULL) {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  return true;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
